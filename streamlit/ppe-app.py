bdec13442bdb86940d024b21a39d7bcef80fe6ec
Raquel Colares <69801403+raquelcolares@users.noreply.github.com>
2025-11-26 15:23:07 -0500

    updating streamlit

--- streamlit/ppe-app.py
+++ streamlit/ppe-app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from PIL import Image
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 from backend import (
     load_simple_cnn, load_resnet50, load_yolo,
